fa3ea8d25b47e629cab0e90bb269ff5a95d6573d
b-juu <erichassett4@gmail.com>
2023-10-02 12:25:21 -0400

    ex1 initial commit

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Try to guess my favorite number");
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > favNumber)
+            {
+                Console.Write("too high");
+            }
+            if (guess < favNumber)
+            {
+                Console.Write("too low");
+            }
+            else Console.Write("You guessed it!");
         }
     }
 }
